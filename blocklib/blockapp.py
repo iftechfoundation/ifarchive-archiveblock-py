c7524fff596d7d0d57f1f335dac6a3c630f317b5
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-02 22:53:10 -0500

    App file.

---
+++ blocklib/blockapp.py
@@ -0,0 +1,14 @@
+import threading
+
+from tinyapp.app import TinyApp, TinyRequest
+from tinyapp.handler import ReqHandler
+
+class BlockApp(TinyApp):
+    """BlockApp: The TinyApp class.
+    """
+    
+    def __init__(self, config, hanclasses):
+        TinyApp.__init__(self, hanclasses)
+
+        # Thread-local storage for various things which are not thread-safe.
+        self.threadcache = threading.local()
