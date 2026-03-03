5d9e8dae3b5524f523cebf94afe474213033f4ad
Andrew Plotkin <zarf@ZarfLent.local>
2026-03-02 23:12:07 -0500

    Grab more config.

--- blocklib/blockapp.py
+++ blocklib/blockapp.py
@@ -10,5 +10,8 @@ class BlockApp(TinyApp):
     def __init__(self, config, hanclasses):
         TinyApp.__init__(self, hanclasses)
 
+        self.blockmappath = config['ArchiveBlock']['ArchiveBlockMapPath']
+        self.restrictdomain = config['ArchiveBlock']['ArchiveBlockRestrictDomain']
+        
         # Thread-local storage for various things which are not thread-safe.
         self.threadcache = threading.local()
